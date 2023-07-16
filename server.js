d1f49d2e59c77bfc557df554a1c9d007a8ca093e
Alex Digrazia <alexndigrazia@hotmail.com>
2023-07-15 21:50:53 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+
+const nodemailer = require("nodemailer");
+
+const PORT = process.env.PORT || 4000;
+
+//Middleware
+app.use(express.static("public"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/contactForm.html");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.office365.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.EMAIL_ADDRESS,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+    tls: {
+      ciphers: "SSLv3",
+      rejectUnauthorized: false, // Allows self-signed certificates
+    },
+  });
+
+  const mailOptions = {
+    from: process.env.EMAIL_ADDRESS,
+    to: process.env.EMAIL_ADDRESS,
+    subject: `Message from ${req.body.email}: ${req.body.subject}`,
+    text: req.body.message,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("success");
+    }
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
